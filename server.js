8689375de466f9c931b3cc39f714393dfdf93c49
amar3012005 <148769827+amar3012005@users.noreply.github.com>
2025-02-04 17:53:45 +0530

    Update server.js

--- server.js
+++ server.js
@@ -26,7 +26,7 @@ app.use(cors({
     'https://foodles.shop',
     'https://www.foodles.shop',
     'https://precious-cobbler-d60f77.netlify.app', // If using Netlify for frontend
-    'http://localhost:3000'                 // Keep local development
+    ''                 // Keep local development
   ],
   credentials: true,
   methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
